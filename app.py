a20820461845b723cf2aa46433b10512b5f4ede7
Josh-Reimer <reimer.josh.2006@gmail.com>
2023-12-26 18:50:15 -0400

    fixed issue #2

--- app.py
+++ app.py
@@ -121,31 +121,65 @@ def add_folder():
 	#what about the folder name
 	return f"{source}"		
 	
+	
+	
 @app.route('/delete_item')
 def delete_item():
 	junk_items = request.args.get("junk_items")
 	if junk_items == "":
 		return "zero junk items"
 	junk_items = junk_items.replace("%20"," ").replace("[","").replace("]","")
+	
 	junk_list = junk_items.split(",")
+	#junk_list is a list of all the items that are going to be deleted. these are not paths, just folder names
+	class Junk_item:
+		trash_path = ""
+		
+		def __init__(self, path,name):
+			self.path = path
+			self.name = name
+			
+	junk_classes = []
+	junk_things_paths = []
+	#junk_things_paths is a list of strings which are paths to all the items that are going to be deleted
 	
 	#parsing junk items names into paths
-	junk_things_paths = []
-	
-	
 	for junk_thing in junk_list:
 		if "collapsible_" in junk_thing:
-			path1 = f"static/manuals/{junk_thing.replace('collapsible_', '')}"
+			name = junk_thing.replace('collapsible_', '')
+			
+			path1 = f"static/manuals/{name}"
+			junk_obj = Junk_item(path1,name)
+			junk_classes.append(junk_obj)
 			junk_things_paths.append(path1)
 			
 		elif "content_|" in junk_thing:
-			path2 = f"static/manuals/{junk_thing.replace('content_|', '').replace('|','/')}"
+			name = junk_thing.replace('content_|', '').replace('|','/')
+			
+			path2 = f"static/manuals/{name}"
+			junk_obj = Junk_item(path2,name.split("/")[1])
+			junk_classes.append(junk_obj)
 			junk_things_paths.append(path2)
 			
-	for item in junk_things_paths:	
-		shutil.move(item, "static/trash")
-		print(item)
 	
+	#items_in_trash is a list of the items already in the trash; these are not paths, just folder names
+	
+	#check if any of the items to be trashed have the same name as one of the already trashed items and rename the next one to be trashed
+	
+	for item in junk_classes:
+		new_name = item.name
+		count = 1
+			
+		while os.path.exists(f"static/trash/{new_name}"):
+				
+			new_name = f"{item.name}_duplicate{count}"
+			
+			count += 1
+				
+		os.mkdir(f"static/trash/{new_name}")			
+		new_path = f"static/trash/{new_name}"
+		shutil.move(item.path,new_path)
+		
 	return "deleting items"	
 	
 @app.route('/upload', methods=['GET','POST'])			
